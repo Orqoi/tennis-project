a39896b26600f39883f87074e329c8afb69b52ba
Orqoi <lewklconrad@gmail.com>
2023-11-27 16:27:08 +0800

    Add result parser

---
+++ parser.py
@@ -0,0 +1,32 @@
+import csv
+import datetime
+
+# Open the input text file for reading and the output CSV file for writing
+with open('results.txt', 'r') as input_file, open('results.csv', 'w', newline='') as output_file:
+    # Create a CSV writer object
+    csv_writer = csv.writer(output_file)
+
+    # Write the header to the CSV file
+    csv_writer.writerow(['date', 'P1Name', 'P2Name', 'P1WinProb', 'P2WinProb'])
+
+    # Read each line from the input text file
+    date = ''
+    P1Name = ''
+    P2Name = ''
+    P1WinProb = ''
+    P2WinProb = ''
+    for line in input_file:
+        # Check if the line contains "pcsp"
+        if '.pcsp' in line:
+            data = line[61:-6].split('_')
+            date = data[0]
+            P1Name = data[1].replace('-', ' ')
+            P2Name = data[2].replace('-', ' ')
+        elif 'Valid with Probability' in line:
+            data = line[87:-3].split(', ')
+            P1WinProb = round((float(data[0]) + float(data[1])) / 2, 4)
+            P2WinProb = 1 - P1WinProb
+            print([date, P1Name, P2Name, P1WinProb, P2WinProb])
+            csv_writer.writerow([date, P1Name, P2Name, P1WinProb, P2WinProb])
+
+print("CSV file 'results.csv' has been created.")
